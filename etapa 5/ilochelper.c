760318e43b12f0d211ffbb3dea4c4419570cf43e
Augusto Timm <augusto.tes@gmail.com>
2019-11-11 13:25:40 -0300

    Criada funcao para soma, sem uso de literais

--- etapa 5/ilochelper.c
+++ etapa 5/ilochelper.c
@@ -1,5 +1,18 @@
 #include "helper.h"
 
+int lastKnownRegister=0;
+
+OpDataList_t* operationsList=NULL;
+
+int newRegister(){
+    lastKnownRegister++;
+    return lastKnownRegister;
+}
+
+OpData_t* createIloc(){
+       OpData_t* newOp = calloc(1, sizeof(OpData_t));
+       return newOp;
+}
 
 OpData_t* nodeToIloc(NodoArvore_t* node, int registerNumber){
     if(node->operation == Iadd){
@@ -16,4 +29,21 @@ OpData_t* nodeToIloc(NodoArvore_t* node, int registerNumber){
 
 
 }
-OpData_t* ifToIloc()
+
+OpData_t* addToIloc(NodoArvore_t* node, int registerNumber){
+    OpData_t* newOp = createIloc();
+    int regUm, regDois;
+    regUm = newRegister();
+    regDois = newRegister();
+    newOp->operation = Iadd;
+    newOp->registerNumberArg1 = regUm;
+    newOp->registerNumberArg2 = regDois;
+    newOp->registerNumberArg3 = registerNumber;
+    NodoArvore_t* childOne = node->children->nodo;
+    NodoArvore_t* childTwo = node->children->next->nodo;
+    nodeToIloc(childOne,regUm);
+    nodeToIloc(childTwo,regDois);
+
+
+}
+
